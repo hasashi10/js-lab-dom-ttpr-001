f97306ce720c481446eca013a9eca8f8a26dd4fa
hasashi10 <hanz15@live.com>
2025-05-06 18:36:21 -0400

    change to toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function toggleDarkMode() {
     // This function should toggle the dark mode class on the body
     // comment out the alert below and replace it with your code
-    alert("change color in this function");
+    document.body.classList.toggle("dark");
+    //alert("change color in this function");
   /*
     TODO: Replace the alert above with code that toggles
     the 'dark' class on <body>.
